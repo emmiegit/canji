cd0fe87d422714039f27604b79819f11898bc5b8
Emmie Maeda <emmie.maeda@gmail.com>
2023-12-23 18:57:34 -0500

    Add script for converting between hex and char form.

---
+++ char_id.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+Converts between characters and their hex representation, and vice eversa.
+"""
+
+import sys
+
+def hex_to_char(s):
+    return chr(int(s, 16))
+
+def char_to_hex(c):
+    return f"{ord(c):05x}"
+
+if __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        if len(arg) == 1:
+            print(char_to_hex(arg))
+        else:
+            print(hex_to_char(arg))
