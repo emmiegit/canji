150855cc7b57d6ead84aef3a7145f7b64a137b16
Emmie Maeda <emmie.maeda@gmail.com>
2023-12-23 20:47:51 -0500

    Export XML_PREFIX_MAP.

--- common.py
+++ common.py
@@ -12,10 +12,15 @@ DEFAULT_WIDTH = 109
 DEFAULT_HEIGHT = 109
 DEFAULT_VIEWBOX = f"0 0 {DEFAULT_WIDTH} {DEFAULT_HEIGHT}"
 
+XML_PREFIX_MAP = {
+    "": "http://www.w3.org/2000/svg",
+    "kvg": "http://kanjivg.tagaini.net",
+}
+
 
 def register_xml_namespaces():
-    ElementTree.register_namespace("", "http://www.w3.org/2000/svg")
-    ElementTree.register_namespace("kvg", "http://kanjivg.tagaini.net")
+    for key, value in XML_PREFIX_MAP.items():
+        ElementTree.register_namespace(key, value)
 
 
 def parse_xml(path):
